fb7d004e7c3de5bb82bc07a3993011e9e8bd863f
Mayuri Gurav <mayurigurav1997@gmail.com>
2022-12-07 18:58:24 +0530

    icecream reducer

--- app/store.js
+++ app/store.js
@@ -1,17 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
-// const reduxLogger = require('redux-logger')
+const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
-// const icecreamReducer = require('../features/icecream/icecreamSlice')
+const icecreamReducer = require('../features/icecream/icecreamSlice')
 // const userReducer = require('../features/user/userSlice')
-// const logger = reduxLogger.createLogger()
+const logger = reduxLogger.createLogger()
 
 const store = configureStore({
+    // dont need to use combine reducer ,we can directly use multiple reducers
     reducer: {
         cake: cakeReducer,
-        // icecream: icecreamReducer,
+        icecream: icecreamReducer,
         // user: userReducer
-    }
-    // middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const store = require("./app/store")
 const cakeActions = require("./features/cake/cakeSlice").cakeActions
+const icecreamActions = require("./features/icecream/icecreamSlice").icecreamActions
 
 console.log("Initial state", store.getState())
 const unsubscribe = store.subscribe(() => {
-    console.log("Updated state", store.getState())
+    // console.log("Updated state", store.getState())
 })
+
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
+store.dispatch(cakeActions.restocked(2))
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(5))
 unsubscribe()
