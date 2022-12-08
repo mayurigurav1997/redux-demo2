334ca8f9bb505219a45becf1c62ba142f841f6c9
Mayuri Gurav <mayurigurav1997@gmail.com>
2022-12-08 20:42:21 +0530

    Added async thunk middleware for fetchusers

--- app/store.js
+++ app/store.js
@@ -2,7 +2,7 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
-// const userReducer = require('../features/user/userSlice')
+const userReducer = require('../features/user/userSlice')
 const logger = reduxLogger.createLogger()
 
 const store = configureStore({
@@ -10,9 +10,9 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
-        // user: userReducer
+        user: userReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
